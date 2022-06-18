e050ca528086d5e03f52a2c5cebd8803d9fa94cb
leod-e1 <leodangeli11@gmail.com>
2022-06-18 00:48:10 -0300

    [update] corrections of page, changes in styles, remove divs

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => fetchData());
+
 const fetchData = async () => {
-    try {
-      const res = await fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
-      const data = await res.json()
-      // laFuncionQueSeCree(data);
-  
-    } catch (error) {
-      console.log(error)
-    }
+  try {
+    const res = await fetch(
+      "https://www.dolarsi.com/api/api.php?type=valoresprincipales"
+    );
+    const data = await res.json();
+    // laFuncionQueSeCree(data);
+  } catch (error) {
+    console.log(error);
   }
+};
