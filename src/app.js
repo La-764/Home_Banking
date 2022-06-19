9882d047c8bd6035e2fa4821d9c739e27c95c6c5
LautyT02 <lautyt02@gmail.com>
2022-06-19 11:15:23 -0300

    modificacion app.js
    Se añadio todo lo necesario para generar las tarjetas: Se trajo por id al template y a la seccion de dolar. Se creo un fragment y se creo la funcion que genera las tarjetas

--- src/app.js
+++ src/app.js
@@ -1,14 +1,41 @@
+const template = document.getElementById('template').content
+const cards=document.getElementById('cards-dolar')
+const fragment= document.createDocumentFragment()
 document.addEventListener("DOMContentLoaded", () => fetchData());
-const dolar=document.getElementById('dolar')
 const fetchData = async () => {
   try {
     const res = await fetch(
       "https://www.dolarsi.com/api/api.php?type=valoresprincipales"
     );
     const data = await res.json();
-    // laFuncionQueSeCree(data);
+    console.log(template)
+    pintarCards(data)
   } catch (error) {
     console.log(error);
   }
 };
+const pintarCards = data => {
+  const fecha = new Date()
+  let variacion
+  //  console.log(data);
+  data.forEach(item => {
+    console.log(item)
+    if(item.casa.variacion==undefined){
+      variacion='no disponible'
+    }
+    else{
+      variacion=item.casa.variacion
+    }
+    template.querySelector('.card-title').textContent = item.casa.nombre
+    template.querySelector('.compra').textContent = item.casa.compra
+    template.querySelector('.venta').textContent = item.casa.venta
+    template.querySelector('.card-subtitle').textContent = "variacion: " + variacion
+    template.querySelector('.fecha').textContent = fecha.getDate()+'/'+(fecha.getMonth()+1)+'/'+fecha.getFullYear()+' '+fecha.getHours()+':'+fecha.getMinutes()
+    const clone = template.cloneNode(true)
+    fragment.appendChild(clone)
+  });
+
+  cards.appendChild(fragment)
+}
+
 
