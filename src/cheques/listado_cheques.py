7d72728691b0446e41cc6c9556eedcfb7b5601f1
Luis <sergioluispagano@gmail.com>
2022-07-03 18:42:47 -0300

    CrearCSV hecho

--- src/cheques/listado_cheques.py
+++ src/cheques/listado_cheques.py
@@ -1,13 +1,23 @@
 import csv
+from datetime import datetime
 from operator import length_hint
 
 archivo = input("Ingrese nombre del archivo csv: ")
 filtrarDni = input("Ingrese DNI a filtrar: ")
 salida=input("Ingrese pantalla si desea  imprimir por “pantalla” todos los valores que se tienen y  “csv” si desea exportar a un csv").upper()
 chequesFiltrados = []
-def crearCSV():
-    with open('ITBA_test.txt', 'w', encoding='utf-8') as cheques:
-        pass
+
+def crearCSV(nombre_archivo, dni):
+    nombre_csv = str(dni) + str(datetime.now())
+    cheques = open(nombre_archivo, 'r')
+    for linea in cheques:
+        DNI = linea.rstrip("\n").split(",")
+        if DNI == dni:
+            NumeroCuenta, Valor, FechaOrigen, FechaPago = linea.rstrip("\n").split(",")
+            csv = open(nombre_csv.csv, "w")
+            csv.write(NumeroCuenta + "," + Valor + "," + FechaOrigen + "," + FechaPago + "\n")
+            csv.close
+    cheques.close()
 
 
 
@@ -30,12 +40,11 @@ print()
 print(name_records[0]['FechaPago'])
 
 
-def filtrarPorDni(filtrarDni):
-    
-    for i in range(0, lenght(documento)):
+def filtrarPorDni():
+    for i in range(0, len(documento)):
         if documento[i] == filtrarDni:
             if salida == "PANTALLA":
                 print("DNI: " + documento[i] + "\n Tipo:" + tipo[i] + "\n Estado:" + estado[i])
             elif salida == "CSV":
-                crearCSV()
+                crearCSV(archivo, filtrarDni)
 
