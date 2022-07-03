07dffec038949d6cca30dea3e5f7e0b779d376d3
dev1-ish <alma.miguez30@gmail.com>
2022-07-03 17:56:05 -0300

    extracción de datos

--- src/cheques/listado_cheques.py
+++ src/cheques/listado_cheques.py
@@ -1,15 +1,26 @@
 import csv
 cheques=[]
 
+
+def crearCSV():
+    with open('ITBA_test.txt', 'w', encoding='utf-8') as cheques:
+        pass
+
+
+
 with open('ITBA_test.txt', 'r', encoding='utf-8') as f:
     csv_reader = csv.DictReader(f)
     name_records = list(csv_reader)
     ## extrae datos
-    documento=name_records[0]['DNI']
+    for line in name_records:
+        nro_cheque = (name_records[line]['NroCheque'])
+        documento = (name_records[line]['DNI'])
+        tipo = (name_records[line]['Tipo'])
+        estado = (name_records[line]['Estado'])
 
     salida=input("Ingrese pantalla si desea  imprimir por “pantalla” todos los valores que se tienen y  “csv” si desea exportar a un csv").upper()
     if salida == "PANTALLA":
-            print("DNI: " + DNI + "\n Tipo:" + Tipo + "\n Estado:" + Estado)
+            print("DNI: " + documento + "\n Tipo:" + tipo + "\n Estado:" + estado)
     elif salida == "CSV":
             crearCSV()
 
@@ -22,6 +33,7 @@ print(name_records[0]['FechaPago'])
 
 
 for linea in f:
+    ## hacer verificación de nro de cheque (ID) con dni
         DNI, Tipo, Estado = linea.rstrip("\n").split(",")
         salida=input("Ingrese pantalla si desea  imprimir por “pantalla” todos los valores que se tienen y  “csv” si desea exportar a un csv").upper()
         if salida == "PANTALLA":
@@ -29,6 +41,3 @@ for linea in f:
         elif salida == "CSV":
             crearCSV()
 
-def crearCSV():
-    with open('ITBA_test.txt', 'w', encoding='utf-8') as  cheques:
-        pass
