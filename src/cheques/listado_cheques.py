301d62669ba3e231193ab6b0334ad974ef219c8a
Luis <sergioluispagano@gmail.com>
2022-08-24 20:55:11 -0300

    Fixing mistakes marked by Yanu (3/4)

--- src/cheques/listado_cheques.py
+++ src/cheques/listado_cheques.py
@@ -7,7 +7,7 @@ archivo = input("Ingrese nombre del archivo csv: ")
 filtrarDni = input("Ingrese DNI a filtrar: ")
 salida = input("Ingrese pantalla si desea  imprimir por “pantalla” todos los valores que se tienen y  “csv” si desea exportar a un csv: ").upper()
 chequesFiltrados = []
-nro_cheque = []
+nroCheque = []
 documento = []
 tipo = []
 estado = []
@@ -16,47 +16,49 @@ ts = datetime.timestamp(dt)
 
 
 def crearCSV(dni, linea):
-    nombre_csv = str(dni)+"_"+str(ts)+".csv"
+    nombreCSV = str(dni)+"_"+str(ts)+".csv"
 
-    NumeroCuenta = name_records[linea]['NumeroCuentaOrigen']
-    valor = name_records[linea]['Valor']
-    FechaOrigen = name_records[linea]['FechaOrigen']
-    FechaPago = name_records[linea]['FechaPago']
+    NumeroCuenta = nameRecords[linea]['NumeroCuentaOrigen']
+    valor = nameRecords[linea]['Valor']
+    FechaOrigen = nameRecords[linea]['FechaOrigen']
+    FechaPago = nameRecords[linea]['FechaPago']
 
-    csv = open(nombre_csv, "a")
+    csv = open(nombreCSV, "a")
     csv.write(documento[linea]+","+NumeroCuenta + "," +
               valor + "," + FechaOrigen + "," + FechaPago + "\n")
 
     csv.close()
 
 
-def error():
+def errorPorNumeroDeCheque():
     if len(chequesFiltrados) != len(set(chequesFiltrados)):
         print(
             "Error: Existe más de un cheque con el mísmo número de cheque para esta cuenta")
 
 
 def filtrarPorDni():
-
-    for i in range(0, len(documento)):
-        if documento[i] == filtrarDni:
-            chequesFiltrados.append(nro_cheque[i])
-            if salida == "PANTALLA":
-                print("DNI: " + documento[i] + "\n Tipo:" +
-                      tipo[i] + "\n Estado:" + estado[i])
-            elif salida == "CSV":
-                crearCSV(filtrarDni, i)
-    error()
+    try:
+        for i in range(0, len(documento)):
+            if documento[i] == filtrarDni:
+                chequesFiltrados.append(nroCheque[i])
+                if salida == "PANTALLA":
+                    print("DNI: " + documento[i] + "\n Tipo:" +
+                    tipo[i] + "\n Estado:" + estado[i])
+                if salida == "CSV":
+                    crearCSV(filtrarDni, i)
+        errorPorNumeroDeCheque()
+    except:
+        raise
 
 
 with open(archivo, 'r', encoding='utf-8') as f:
-    csv_reader = csv.DictReader(f)
-    name_records = list(csv_reader)
+    csvReader = csv.DictReader(f)
+    nameRecords = list(csvReader)
 
-    for line in range(0, len(name_records)):
-        nro_cheque.append(name_records[line]['NroCheque'])
-        documento.append(name_records[line]['DNI'])
-        tipo.append(name_records[line]['Tipo'])
-        estado.append(name_records[line]['Estado'])
+    for line in range(0, len(nameRecords)):
+        nroCheque.append(nameRecords[line]['NroCheque'])
+        documento.append(nameRecords[line]['DNI'])
+        tipo.append(nameRecords[line]['Tipo'])
+        estado.append(nameRecords[line]['Estado'])
 
     filtrarPorDni()
